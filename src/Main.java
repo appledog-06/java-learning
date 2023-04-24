fd40d2e948f7efb79f70ee0befbd6dad8e275828
appledog-06 <surbikarki@hotmail.com>
2023-04-24 17:31:20 +0545

    repo initialize

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+       int x=123;
+       char y='@';
+       boolean z=true;
+       String a="surbi";
+       double b=2.1433;
+       float c=3.14f;
+       System.out.println(x+"hello"+a+y+z+b+c);
+       //swapping of two variables
+        String w="water";
+        String t="ice";
+        String temp;
+        temp=w;
+        w=t;
+        t=temp;
+        System.out.println("w:"+w);
+        System.out.println("t:"+t);
+
+
+    }
+}
