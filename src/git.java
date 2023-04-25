b8bc49bcd2a25720ed595b1eea725022b58aafed
surbi karki <surbikarki@hotmail.com>
2023-04-25 08:37:45 +0545

    Add files via upload

--- src/git.java
+++ src/git.java
@@ -1,5 +1,17 @@
-public class git {
+import java.util.Scanner;
+public class git{
     public static void main(String[]args){
-        System.out.printf("welcome to github");
+        Scanner x=new Scanner (System.in);
+        System.out.println("what is your name?");
+        String name=x.nextLine();
+        System.out.println("how old are you?");
+        int age=x.nextInt();
+        x.nextLine();
+        System.out.println("what is your favorite color?");
+        String color=x.nextLine();
+        System.out.println("hello"+" "+name);
+        System.out.println("you are"+" "+age+"years old");
+        System.out.println("you like"+color);
+
     }
 }
